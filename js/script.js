a75c32fc474d4f0e146730cea1d4a0bfc228b6b3
GSNascimento <68621501+GSNascimento@users.noreply.github.com>
2020-08-24 01:22:55 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -108,11 +108,11 @@ $ajaxUtils.sendGetRequest(
       // Hint: you need to surround the chosen category short name with something before inserting
       // it into the home html snippet.
       //
-      //var homeHtmlToInsertIntoMainPage = insertHtml("#main-content",homeHtml);
-      homeHtml= insertProperty(homeHtml,"randomCategoryShortName",chosenCategoryShortName);
-      var homeHtmlToInsertIntoMainPage = insertHtml("#main-content",homeHtml);
-
-
+       var homeHtmlToInsertIntoMainPage = insertHtml("#main-content",homeHtml);
+       homeHtml= insertProperty(homeHtml,"randomCategoryShortName",chosenCategoryShortName);
+       homeHtmlToInsertIntoMainPage = insertHtml("#main-content",homeHtml);
+    
+     
       // TODO: STEP 4: Insert the the produced HTML in STEP 3 into the main page
       // Use the existing insertHtml function for that purpose. Look through this code for an example
       // of how to do that.
